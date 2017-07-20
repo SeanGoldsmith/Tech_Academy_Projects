1a4c9820243e12376dc835c6a0fd9a815c15b73f
Sean <goldsmith.sean90@gmail.com>
2017-07-20 16:11:42 -0700

    Adding range() drill to python folder

---
+++ python/range_drill.py
@@ -0,0 +1,12 @@
+#Drill 1
+print ("Drill 1")
+for i in range(4):
+    print (i)
+#Drill 2
+print ("\nDrill 2")
+for j in range(3,-1,-1):
+    print (j)
+#Drill 3
+print ("\nDrill 3")
+for k in range (8,0,-2):
+    print (k)
