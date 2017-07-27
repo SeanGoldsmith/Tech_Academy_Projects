4e33e1263bc23140b1813f0de9decec527b9e0ab
Sean <goldsmith.sean90@gmail.com>
2017-07-27 13:51:05 -0700

    Making changes to python drill three.  Using timdelta and timestamp to make drill more accurate.

--- python/Drills/drill3.py
+++ python/Drills/drill3.py
@@ -2,20 +2,19 @@ from datetime import *
 import os.path
 import shutil as sH
 
-path_to_dir = 'C:\\Users\\seang\\Desktop\\oldFiles\\'
+path_to_dir = 'C:\\Users\\My Elite\Desktop\\oldFiles\\'
 
-path_to_copy = 'C:\\Users\\seang\\Desktop\\transfer'
+path_to_copy = 'C:\\Users\\My Elite\\Desktop\\transfer\\'
 
 current = datetime.now()
-
 
 
 def copyNew(file_list):
     for each in file_list:
         mtime = os.path.getmtime(path_to_dir + each)
         date = datetime.fromtimestamp(mtime)
-        print ('Month: {}, Day: {}, Year: {}').format(date.month,date.day,date.year)
-        if date.day == current.day:
+        print ('Month: {}, Day: {}, Year: {}'.format(date.month,date.day,date.year))
+        if date > (current-timedelta(1)):
             sH.copy(path_to_dir + each,
                     path_to_copy)
             
