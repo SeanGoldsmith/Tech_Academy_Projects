b8377b02be94095b6b64839ca98c8ca4dc519bb9
Sean <goldsmith.sean90@gmail.com>
2017-08-07 16:34:32 -0700

    Adding conditional radio button challenge to repos

---
+++ cSharp/conditionalRadioChallenge/conditionalRadioChallenge/Default.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="conditionalRadioChallenge.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Your Note Taking Preferences<br />
+        </div>
+        <asp:RadioButton ID="pencilButton" runat="server" GroupName="note" Text="Pencil" />
+        <br />
+        <asp:RadioButton ID="penButton" runat="server" GroupName="note" Text="Pen" />
+        <br />
+        <asp:RadioButton ID="phoneButton" runat="server" GroupName="note" Text="Phone" />
+        <br />
+        <asp:RadioButton ID="tabletButton" runat="server" GroupName="note" Text="Tablet" />
+        <br />
+        <br />
+        <asp:Button ID="okButton" runat="server" OnClick="okButton_Click" Text="OK" />
+        <br />
+        <br />
+        <asp:Image ID="noteImage" runat="server" />
+        <br />
+        <br />
+        <asp:Label ID="outputLabel" runat="server" Text="Label"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ cSharp/conditionalRadioChallenge/conditionalRadioChallenge/Default.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace conditionalRadioChallenge
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            if (pencilButton.Checked)
+            {
+                outputLabel.Text = "You selected the pencil.";
+                noteImage.ImageUrl = "pencil.png";
+                
+            }
+            else if (penButton.Checked)
+            {
+                outputLabel.Text = "You selected the pen.";
+                noteImage.ImageUrl = "pen.png";
+            }
+            else if (phoneButton.Checked)
+            {
+                outputLabel.Text = "You Selected the phone.";
+                noteImage.ImageUrl = "phone.png";
+            }
+            else if (tabletButton.Checked)
+            {
+                outputLabel.Text = "You selected the tablet.";
+                noteImage.ImageUrl = "tablet.png";
+            }
+            else
+            {
+                outputLabel.Text = "Please make a selection.";
+            }
+        }
+    }
+}
