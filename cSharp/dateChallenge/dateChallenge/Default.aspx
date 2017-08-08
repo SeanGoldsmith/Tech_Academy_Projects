866af09b3e7b3ae3e032cd877ff40311bb153be4
Sean <goldsmith.sean90@gmail.com>
2017-08-08 15:28:44 -0700

    Adding datetime challenge to c# repos

---
+++ cSharp/dateChallenge/dateChallenge/Default.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="dateChallenge.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            How many days have elapsed?<br />
+            <br />
+            Chose a date:<br />
+            <asp:Calendar ID="firstCal" runat="server"></asp:Calendar>
+            <br />
+            <br />
+            Chose a second date:<br />
+            <asp:Calendar ID="secondCal" runat="server"></asp:Calendar>
+            <br />
+            <br />
+            <asp:Button ID="okButton" runat="server" OnClick="okButton_Click" Text="OK" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ cSharp/dateChallenge/dateChallenge/Default.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace dateChallenge
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            DateTime firstDay = firstCal.SelectedDate;
+            DateTime secondDay = secondCal.SelectedDate;
+
+            TimeSpan diff = firstDay - secondDay;
+            if (diff.Days > 0)
+            {
+                resultLabel.Text = diff.Days.ToString();
+            }
+            else if (diff.Days < 0)
+            {
+                resultLabel.Text = (diff.Days * -1).ToString();
+            }
+            else
+            {
+                resultLabel.Text = "0";
+            }
+        }
+    }
+}
