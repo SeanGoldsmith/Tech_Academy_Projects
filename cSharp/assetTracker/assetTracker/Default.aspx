c1fc866735fcade775243f09e58e5679d52ecac0
Sean <Goldsmith.sean90@gmail.com>
2017-08-09 18:01:45 -0700

    Adding c# item 18 to repos

---
+++ cSharp/assetTracker/assetTracker/Default.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="assetTracker.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .newStyle1 {
+            height: 150px;
+            width: 100px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Image ID="Image1" runat="server" CssClass="newStyle1" ImageUrl="epic-spies-logo.jpg" />
+        </div>
+        <h1>Asset Performance Tracker</h1>
+        <p>
+            Asset Name:&nbsp;
+            <asp:TextBox ID="assetBox" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            Elections Rigged:&nbsp;
+            <asp:TextBox ID="electionBox" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            Acts of Subterfuge Performed:
+            <asp:TextBox ID="subBox" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+        </p>
+        <p>
+            <asp:Label ID="outputLabel" runat="server"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ cSharp/assetTracker/assetTracker/Default.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace assetTracker
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                string[] assets = new string[0];
+                int[] elections = new int[0];
+                int[] subt = new int[0];
+
+                ViewState.Add("Assets", assets);
+                ViewState.Add("Elections", elections);
+                ViewState.Add("Subt", subt);
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string[] assets = (string[])ViewState["Assets"];
+            int[] elections = (int[])ViewState["Elections"];
+            int[] subt = (int[])ViewState["Subt"];
+
+            int newLength = assets.Length + 1;
+
+            Array.Resize(ref assets, newLength);
+            Array.Resize(ref elections, newLength);
+            Array.Resize(ref subt, newLength);
+
+            int newIndex = assets.GetUpperBound(0);
+
+            assets[newIndex] = assetBox.Text;
+            elections[newIndex] = int.Parse(electionBox.Text);
+            subt[newIndex] = int.Parse(subBox.Text);
+
+            ViewState["Assets"] = assets;
+            ViewState["Elections"] = elections;
+            ViewState["Subt"] = subt;
+
+            outputLabel.Text = String.Format("Total Election Rigged: {0}<br>" +
+                "Average Acts of Subterfuge: {1:N2}<br>" +
+                "(Last Asset You Added: {2})",elections.Sum(),subt.Average(),assets[newIndex]);
+
+            assetBox.Text = "";
+            electionBox.Text = "";
+            subBox.Text = "";
+        }
+    }
+}
