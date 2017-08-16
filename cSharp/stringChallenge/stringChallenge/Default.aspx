ce5227c289152a654d7b0a0f5bc114ecdcad4da3
Sean <Goldsmith.sean90@gmail.com>
2017-08-16 16:40:55 -0700

    Adding
    Adding string challenge to repository

---
+++ cSharp/stringChallenge/stringChallenge/Default.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="stringChallenge.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Part One:<br />
+            <asp:Label ID="firstLabel" runat="server"></asp:Label>
+            <br />
+            <br />
+            Part Two:<br />
+            <asp:Label ID="secondLabel" runat="server"></asp:Label>
+            <br />
+            <br />
+            Part Three:<br />
+            <asp:Label ID="thirdLabel" runat="server"></asp:Label>
+            <br />
+            <br />
+            Part Four:<br />
+            <asp:Label ID="fourthLabel" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ cSharp/stringChallenge/stringChallenge/Default.aspx.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace stringChallenge
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            challengOne();
+            challengeTwo();
+            challengeThree();
+            challengeFour();
+
+            
+
+            
+        }
+
+        void challengOne()
+        {
+            //PART 1
+            string myName = "Sean Goldsmith";
+            string revname = "";
+
+            for (int i = myName.Length - 1; i >= 0; i--)
+            {
+                revname += myName[i];
+            }
+            firstLabel.Text = revname;
+            //END PART 1
+        }
+
+        void challengeTwo()
+        {
+            //PART TWO
+            string names = "Luke,Leia,Han,Chewbacca";
+            string[] namesList = names.Split(',');
+            string newNames = "";
+            for (int i = namesList.Length - 1; i >= 0; i--)
+            {
+                newNames += namesList[i] + ",";
+            }
+            newNames = newNames.Remove(newNames.Length - 1);
+            secondLabel.Text = newNames;
+            //END PART TWO
+        }
+
+        void challengeThree()
+        {
+            int forLeftPad = 0;
+            string names = "Luke,Leia,Han,Chewbacca";
+            string[] namesList = names.Split(',');
+            string output = "";
+            for (int i = 0;i < namesList.Length; i++)
+            {
+                forLeftPad = ((14 - namesList[i].Length) / 2);
+                output += namesList[i].PadLeft(namesList[i].Length+forLeftPad, '*').PadRight(14,'*') + "<br>";
+            }
+            thirdLabel.Text = output;
+
+        }
+
+        void challengeFour()
+        {
+            string puzzle = "NOW IS ZHEremove-me ZIME FOR ALL GOOD MEN ZO COME ZO ZHE AID OF ZHEIR COUNZRY.";
+            puzzle = puzzle.Replace("Z", "T");
+            int index = puzzle.IndexOf("remove-me", 0, puzzle.Length);
+            puzzle = puzzle.Remove(index, 9);
+            puzzle = puzzle.Trim();
+            puzzle = puzzle.ToLower();
+            puzzle = puzzle.Remove(0, 1);
+            puzzle = puzzle.Insert(0, "N");
+            fourthLabel.Text = puzzle;
+        }
+
+
+    }
+}
